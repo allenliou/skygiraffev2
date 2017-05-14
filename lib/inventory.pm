29303c551605b882794c8e3fdd04b558245b26b4
allenliou <allenl@outlook.com>
2017-05-13 23:50:57 -0700

    t

--- lib/inventory.pm
+++ lib/inventory.pm
@@ -70,10 +70,11 @@ post '/pick' => sub {
 
   my $dbh = get_connection();
 
-  my $sql = $dbh->prepare("SELECT room_id FROM rooms WHERE room_name=?");
+  my $sql = $dbh->prepare("SELECT room_id FROM rooms WHERE room_name="?"");
   $sql->execute($roomName);
   my $roomId = $sql->fetchall_hashref('room_id');
 
+  # my $sql = $dhh->prepare("SELECT * FROM bookings where room_id=?"")
 
 
   my $timest = localtime();
